988f171eed714e589b6d02ac09712d3696367bc3
Developer Experience team at Ignite <hello@ignite.com>
2025-05-24 08:51:31 +0000

    Initialized with Ignite CLI

---
+++ cmd/Blockchaind/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	clienthelpers "cosmossdk.io/client/v2/helpers"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/longdevbf/Blockchain/app"
+	"github.com/longdevbf/Blockchain/cmd/Blockchaind/cmd"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, clienthelpers.EnvPrefix, app.DefaultNodeHome); err != nil {
+		fmt.Fprintln(rootCmd.OutOrStderr(), err)
+		os.Exit(1)
+	}
+}
